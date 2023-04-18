5ba249cb8668e64e0b4d65bb972f1958d8e8c2d7
Pdb-1807 <purushottambhade18@gmail.com>
2023-04-18 09:54:29 +0530

    1st comment

---
+++ ftweb27-logical-operator-1/problem1.js
@@ -0,0 +1,11 @@
+let dream_mobile="iphone";
+let electronic_store="oneplus";
+if(electronic_store=="dream_mobile")
+{
+  console.log("buy phone");
+}
+else{
+  console.log("not buy");
+  
+}
+(electronic_store=="dream_mobile")
